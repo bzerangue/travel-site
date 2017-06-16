c8aa8c514a71d961ca3ad14b0254874d71b1284c
Brian Zerangue <brian.zerangue@gmail.com>
2017-06-15 20:43:00 -0500

    added gulpfile.js and tested out first gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+gulp.task('default', function(){
+    console.log("hooray, you created a default task.");
+});
+
+gulp.task('html', function(){ 
+    console.log("imagine, html task here.");
+});
