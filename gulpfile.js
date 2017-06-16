12726385b490c545612916623e8292d31d2ecfcd
Brian Zerangue <brian.zerangue@gmail.com>
2017-06-15 20:51:15 -0500

    added styles assets folder; and added gulp-watch to dev dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    watch = require('gulp-watch');
 
 gulp.task('default', function(){
     console.log("hooray, you created a default task.");
@@ -6,4 +7,20 @@ gulp.task('default', function(){
 
 gulp.task('html', function(){ 
     console.log("imagine, html task here.");
+});
+
+gulp.task('styles', function(){ 
+    console.log("imagine, postcss tasks running here.");
+});
+
+gulp.task('watch', function(){
+    
+    watch('./app/index.html', function(){
+        gulp.start('html');
+    });
+
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('styles');
+    });
+
 });
