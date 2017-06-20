860e620c057b8de753f8bc3ac4d615a339434423
Brian Zerangue <brian.zerangue@gmail.com>
2017-06-19 20:37:52 -0500

    made first module with webpack; also, added in jquery via webpack;

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,11 @@
-Person = require('./modules/Person');
+var $ = require('jquery');
+var Person = require('./modules/Person');
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
+
+
+// $("h1").remove();
